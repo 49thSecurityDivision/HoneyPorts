93e013d671f4695bd4fa9a0c762d808ba2c661bf
wonkeymonkey <pettymw@gmail.com>
2017-02-21 02:12:49 -0500

    First Version Done (Needs to be tested)

--- linuxports.bash
+++ linuxports.bash
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+running=1
 portlist="4000 4001 4002"
 
 declare -a children=()
 
 for port in $portlist
 do
-	nc -l -p $port & children+=" $!"
+	echo $port | ./linuxports.helper.bash &
+	children+=" $!"
 done
 
-echo $children
+trap ctrl_c INT
+
+function ctrl_c() {
+	echo "Force Quitting..."
+	for pid in $children
+	do
+		kill $pid
+	done
+	killall nc
+	exit
+}
+
+echo "Press Enter To Quit..."
+read
+
+echo "Quitting..."
 for pid in $children
 do
 	kill $pid
 done
+killall nc
---
+++ linuxports.helper.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+running=1
+
+read port
+
+while [ running ] 
+do
+	nc -l -p $port
+	echo "Port: $port" >> linuxports.log
+done
